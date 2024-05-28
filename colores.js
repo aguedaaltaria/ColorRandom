5c6c2dae598855868719d652b939ccbc102c61b5
aguedaaltaria <156155201+aguedaaltaria@users.noreply.github.com>
2024-05-27 21:19:20 -0700

    Clear list button
    I added a clear list button and change the background

--- colores.js
+++ colores.js
@@ -63,7 +63,7 @@ function addFav0() {
             </span>
             <span class="colorFav">${codigo1}</span>
         </div>
-        <img src="cancel.png" class="closeBtn">
+        <img src="cancel.svg" class="closeBtn">
         <hr></div>
     `;
 };
@@ -79,7 +79,7 @@ function addFav1() {
             </span>
             <span class="colorFav">${codigo1}</span>
         </div>
-        <img src="cancel.png" class="closeBtn">
+        <img src="cancel.svg" class="closeBtn">
         <hr></div>
     `;
 };
@@ -95,7 +95,7 @@ function addFav2() {
             </span>
             <span class="colorFav">${codigo1}</span>
         </div>
-        <img src="cancel.png" class="closeBtn">
+        <img src="cancel.svg" class="closeBtn">
         <hr></div>
     `;
 };
@@ -118,3 +118,10 @@ function desmarcar() {
         a.checked = false;
     }
 };
+
+function borrarLista() {
+    list.innerHTML = `
+    <p>Colores Favoritos</p>
+    <button id="borrarLista" onclick="borrarLista()"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#b67344"><path d="M690-240h190v80H610l80-80Zm-500 80-85-85q-23-23-23.5-57t22.5-58l440-456q23-24 56.5-24t56.5 23l199 199q23 23 23 57t-23 57L520-160H190Zm296-80 314-322-198-198-442 456 64 64h262Zm-6-240Z"/></svg></button>
+    `;
+}
